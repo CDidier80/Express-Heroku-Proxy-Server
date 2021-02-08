16a4e822487539a9320adc2f26219df70148f825
Collin Didier <collin.didier80@gmail.com>
2021-02-07 19:11:06 -0600

    proxy server added with pycore route

---
+++ PycoreRouter.js
@@ -0,0 +1,27 @@
+const Router = require("express").Router()
+const axios = require('axios');
+
+// const API_USAGE_CHECK_ENDPOINT = "https://cors-anywhere.herokuapp.com/https://api.jdoodle.com/v1/credit-spent"
+
+const ApiClient = axios.create({
+    baseURL: 'https://api.jdoodle.com/v1/execute',
+    // headers: { 'X-Auth-Token' : '<some-token>'}
+})
+
+
+const jdoodleSecrets = {
+    clientId : process.env.JDOODLE_CLIENT_ID,
+    clientSecret : process.env.JDOODLE_CLIENT_SECRET
+}
+
+Router.post('/execute', async (req, res) => {
+    try {
+        const jdoodlePayload = {...req.body, ...jdoodleSecrets}
+        const response = await ApiClient.post('', jdoodlePayload)
+        res.send(response.data)
+    } catch (error) {
+        console.log("TRY{}CATCH{} ERROR --> ", error)
+    }
+})
+
+module.exports = Router
---
+++ server.js
@@ -0,0 +1,29 @@
+require("dotenv").config()
+const PycoreRouter = require("./PycoreRouter")
+const bodyParser = require('body-parser')
+const express = require("express")
+const helmet = require('helmet')
+const logger = require('morgan')
+const path = require('path')
+const cors = require('cors')
+
+const app = express()
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(logger('dev'))
+app.use(helmet({ contentSecurityPolicy: false }))  
+app.use(cors())
+app.disable('X-Powered-By') 
+app.use(express.static(path.join(__dirname, 'client', 'build')))   
+
+console.log(PycoreRouter)
+app.use('/pycore', PycoreRouter)
+
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+)
+
+app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
