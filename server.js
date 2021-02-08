2735fd7537fbcf637f4527cfcd52a37c4766dc81
Collin Didier <collin.didier80@gmail.com>
2021-02-07 20:10:39 -0600

    add test route

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const cors = require('cors')
 const app = express()
 const PORT = process.env.PORT || 3001
 
-app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(logger('dev'))
@@ -19,11 +18,11 @@ app.use(cors())
 app.disable('X-Powered-By') 
 app.use(express.static(path.join(__dirname, 'client', 'build')))   
 
-console.log(PycoreRouter)
 app.use('/pycore', PycoreRouter)
+app.get("/test", (req,res)=> res.send("collin-didier-proxy connected"))
 
-app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-)
+// app.get('*', (req, res) =>
+//     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+// )
 
 app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
